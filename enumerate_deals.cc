46caf4bbcd085a35d43f34393d2cc7aec89cbd7d
RubeRad <ruberad@gmail.com>
2019-02-06 21:13:02 -0800

    Implement choose function, and store choose values we will need

--- enumerate_deals.cc
+++ enumerate_deals.cc
@@ -4,17 +4,25 @@
 using std::cout;
 using std::endl;
 
-typedef long long uint64;
+typedef unsigned long long uint64;
 
-void enumerate(int n) {
-  uint64 NUM_DEALS = 1;
-  for (int i=1; i<=n; ++i) {
-    int topno = 81-(i-1);
-    NUM_DEALS *= topno;
-    NUM_DEALS /= i;
+uint64 CHOOSE[82][13];
+
+uint64 choose(int n, int k) {
+  if (k==0) return 0; // base cases
+  if (k==1) return n;
+  uint64 c=1;
+  for (int i=0; i<k; ++i) {
+    c *= n-i; // careful order of multiplication/division
+    c /= i+1; // ensures we always have an integer
   }
+  return c;
+}
 
-  cout << "Num deals for " << n << " cards is " << NUM_DEALS << endl;
+void enumerate(int k) {
+  uint64 NUM_DEALS = CHOOSE[81][k];
+  cout << "Num deals for " << k << " cards is " << NUM_DEALS << endl;
+
   int count=0, sign=1;
   for (uint64 deali=0; deali<NUM_DEALS; ++deali) {
     ++count;
@@ -24,10 +32,14 @@ void enumerate(int n) {
 
 
 int main(int argc, char**argv) {
+
+  for   (int n=0; n<=81; ++n)
+    for (int k=0; k<=12; ++k)
+      CHOOSE[n][k] = choose(n,k);
+
   for (int i=1; i<argc; ++i) {
-    int n = atoi(argv[i]);
+    int k = atoi(argv[i]);
 
-    enumerate(n);
-
+    enumerate(k);
   }
 }
